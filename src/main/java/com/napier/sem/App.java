047bd3fff171e596aaebf23cafdd75aa6e32867b
noxville3 <noxville3@hotmail.co.uk>
2020-01-21 14:46:26 +0000

    dev_branch_tes

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,6 +3,6 @@ package com.napier.sem;
 public class App {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        System.out.println("testest");
     }
 }
