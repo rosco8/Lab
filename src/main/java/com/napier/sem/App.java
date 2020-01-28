447a5c141610fcc44e6ad7703e66e4d588ebe4b9
Nadia Logan <nadialogan@Nadias-MacBook-Pro.local>
2020-01-28 10:21:31 +0000

    changed the app log message for test

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,6 +3,6 @@ package com.napier.sem;
 public class App {
     public static void main(String[] args)
     {
-        System.out.println("dev_branch");
+        System.out.println("hello this is our project.");
     }
 }
